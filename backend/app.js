35deaaf82e68b7484d410cd4f919ae9373aa42bd
Vlada Goreva <mail@vgoreva.ru>
2023-09-04 20:13:34 +0300

    Test

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 const app = express();
 
 app.use(cors({
-  origin: 'https://goreva.nomoredomainsicu.ru',
+  origin: 'https://api.goreva.nomoredomainsicu.ru',
   credentials: true,
 }));
 
