b962d881bb2298b7b860beabf3240a78a079bc55
Vlada Goreva <mail@vgoreva.ru>
2023-09-06 11:26:38 +0300

    crashtest

--- backend/app.js
+++ backend/app.js
@@ -32,6 +32,12 @@ app.use(requestLogger);
 
 app.use(limiter);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/', routes);
 
 app.use(errorLogger);
